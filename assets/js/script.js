5b3e2f4582ab1a5f1a6166f610a9014eb77f66e1
Sp3690 <psmit5259@gmail.com>
2025-05-02 17:57:53 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.getElementById('nav-menu');
+  
+    menuToggle.addEventListener('click', () => {
+      navMenu.classList.toggle('active');
+    });
+  });
+  
